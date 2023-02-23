b2217aa367bb63301c7e2067695e840f30dd9e86
Dhanushka.Bandara <Dhanushka.Bandara@acumatica.com>
2023-02-23 22:08:44 +0530

    Added react - user list not showing

---
+++ ReactUser/ClientApp/src/components/UserData.js
@@ -0,0 +1,57 @@
+﻿import React, { Component } from 'react';
+
+export class UserData extends Component {
+    static displayName = UserData.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateWeatherData();
+    }
+
+    static renderForecastsTable(forecasts) {
+        return (
+            <table className="table table-striped" aria-labelledby="tableLabel">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {forecasts.map(forecast =>
+                        <tr key={forecast.id}>
+                            <td>{forecast.id}</td>
+                            <td>{forecast.name}</td>
+                            <td>{forecast.age}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : UserData.renderForecastsTable(this.state.forecasts);
+
+        return (
+            <div>
+                <h1 id="tableLabel">User Data</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateWeatherData() {
+        const response = await fetch('https://localhost:7119/api/User', {mode:'no-cors'});
+        const data = await response.json();
+        this.setState({ forecasts: data, loading: false });
+    }
+}
